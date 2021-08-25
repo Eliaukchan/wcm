7cbecaa55b5f5d2d257464a3a7bfe36ef425c18e
acdc <2903408591@qq.com>
2021-08-25 16:41:28 +0800

    这个分支用于保存学习vue的代码，请勿合并到master中

--- fixed.js
+++
@@ -1,3 +0,0 @@
-console.log("Hello Git!")
-console.log("I am the master")
-console.log("changeTest!")
---
+++ src/index.js
@@ -0,0 +1,9 @@
+//1.使用ES6的模块化的语法导入jquery
+import $ from 'jquery'
+
+//2.实现隔行变色效果
+
+$(function() {
+    $('li:odd').css('backgroundColor','red')
+    $('li:even').css('backgroundColor','cyan')
+})
