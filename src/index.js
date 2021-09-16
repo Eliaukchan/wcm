078290826f5d729a6c377e0b9d2ddb89be30766f
acdc <2903408591@qq.com>
2021-09-16 10:48:46 +0800

    npm了两个插件，webpack—server和webpack-plugin

--- src/index.js
+++ src/index.js
@@ -5,5 +5,5 @@ import $ from 'jquery'
 
 $(function() {
     $('li:odd').css('backgroundColor','red')
-    $('li:even').css('backgroundColor','cyan')
+    $('li:even').css('backgroundColor','yellow')
 })
